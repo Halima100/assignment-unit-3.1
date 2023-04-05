dd29c5d0faf8a4b81e69cc07a054c2dc183ac5ea
Halima Omar <omarhalima100@gmail.com>
2023-04-04 21:24:00 -0500

    answered question 3

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,11 +8,11 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
-
+let favFoodArray = ['pizza', 'tacos', 'fried rice', 'chicken tikka masala']
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
-
+console.log('My favorite foods are:', favFoodArray);
 
 
 // 2. Array.length - tells you how many items are in the array
@@ -23,7 +23,7 @@ console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
 
-
+console.log('Number of favorite foods', favFoodArray.length);
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
 
@@ -31,14 +31,13 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
-
+console.log('Second animal is', animalArray[1])
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-
-
+console.log('Last animal is', animalArray[3])
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
-
+console.log('Last animal is', animalArray [animalArray.length - 1])
 
 
 // 4. Adding & Removing Array Items
